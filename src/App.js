2dd08c72aeaa7708136679bce0f204d2230c4601
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-01-19 00:03:51 +0530

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world, This is React</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
 }
