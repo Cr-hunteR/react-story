1006ff67ada2b613302f95b9f340e33cce6d6452
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-01-19 00:30:29 +0530

    children property

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>hello world, This is React</h1>
       <Person name='kamal' age='19'></Person>
-      <Person name='dean' age='16'></Person>
+      <Person name='dean' age='16'>This is some extra info about dean.</Person>
       <Person name='jesse' age='20'></Person>
     </div>
   );
