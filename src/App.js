5c43ca8c4d34b4cf592b3649b07edd08e98b6722
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-01-27 09:22:34 +0530

    state changer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component  {
-  
+  state={
+    persons:[
+      {name:'janet', age:23},
+      {name:'jane', age:15},
+      {name:'roy', age:34}
+    ]
+  }
+  stateChanger=()=>{
+    //this.setState.persons[0].name='jesse';
+    this.setState({
+      persons:[
+        {name:'jensen', age:18},
+        {name:'jane', age:15},
+        {name:'roy', age:34}
+      ]
+    })
+  }
   render(){
     return (
       <div className="App">
         <h1>hello world, This is React</h1>
         <Person name='kamal' age='19'></Person>
-        <Person name='dean' age='16'>This is some extra info about dean.</Person>
-        <Person name='jesse' age='20'></Person>
-        <Person name='janet' age='18'></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>This is some extra info about dean.</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        <button onClick={this.stateChanger}>Changer</button>
       </div>
     );
   }
