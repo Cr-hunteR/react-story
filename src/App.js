8c37cc6eff35847f5f04188bf0c48565f4201e77
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-01-27 08:59:30 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello world, This is React</h1>
-      <Person name='kamal' age='19'></Person>
-      <Person name='dean' age='16'>This is some extra info about dean.</Person>
-      <Person name='jesse' age='20'></Person>
-    </div>
-  );
+class App extends Component  {
+  
+  render(){
+    return (
+      <div className="App">
+        <h1>hello world, This is React</h1>
+        <Person name='kamal' age='19'></Person>
+        <Person name='dean' age='16'>This is some extra info about dean.</Person>
+        <Person name='jesse' age='20'></Person>
+        <Person name='janet' age='18'></Person>
+      </div>
+    );
+  }
+  
 }
-
 export default App;
